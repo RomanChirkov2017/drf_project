7845a84e1aa52974cb8f4c076d21c6d65cd36c01
RomanChirkov2017 <138663746+RomanChirkov2017@users.noreply.github.com>
2024-05-23 11:18:41 +1000

    home_work_24_2(исправлено)

--- materials/serializers.py
+++ materials/serializers.py
@@ -12,7 +12,7 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
-    lessons = LessonSerializer(source='lesson_set', many=True)
+    lessons = LessonSerializer(source='lesson_set', many=True, required=False)
 
     def get_lesson_count(self, instance):
         return instance.lesson_set.count()

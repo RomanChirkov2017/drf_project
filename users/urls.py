b1070af38b7fc5def612c51a07bc1976f72c9986
RomanChirkov2017 <138663746+RomanChirkov2017@users.noreply.github.com>
2024-05-25 13:30:56 +1000

    home_work_24_2(исправлено замечание)

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import PaymentsViewSet
 
 app_name = UsersConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
 
 router = routers.SimpleRouter()
 router.register("payments", PaymentsViewSet)
